85d2193a784199a7df5739d50654b1615ddaade2
Paul Sattaur <paulsattaur@hotmail.com>
2025-08-09 20:46:42 -0400

    Deleting test module used in setting Windows env vars

--- SkyVerify_Config.py
+++
@@ -1,8 +0,0 @@
-﻿import sys, json, os, pathlib
-sys.path.insert(0, r"C:\Users\Anima\Dropbox\Sky&P File Archive\Python Programming\SkyServer System\1SkyServer")
-import SkyServer_Core as core  # type: ignore
-cfg = core.load_config()
-print("Resolved base_path:", cfg.get("base_path"))
-print("SIM_MODE:", cfg.get("SIM_MODE"))
-print("Heartbeat path:", cfg.get("heartbeat_log"))
-
