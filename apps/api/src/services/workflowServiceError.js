20194f9e9810def951c184bc9957be8062dc41e2
Paul Sattaur <paulsattaur@hotmail.com>
2026-07-22 23:20:21 -0400

    Phase 15.7.1 - Validation failure bug fixed

---
+++ apps/api/src/services/workflowServiceError.js
@@ -0,0 +1,12 @@
+class WorkflowServiceError extends Error {
+  constructor(message, statusCode = 500, details = {}) {
+    super(message);
+    this.name = 'WorkflowServiceError';
+    this.statusCode = statusCode;
+    this.details = details;
+  }
+}
+
+module.exports = {
+  WorkflowServiceError,
+};
